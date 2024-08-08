8d9dfe303e82593c117d1f547b92824dcdba3409
harvey bj <harveybj@yahoo.com>
2024-08-08 10:12:22 -0700

    FoodItem navigation improvements.

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,25 +1,26 @@
 import { useEffect, useState } from "react";
+import styles from "./search.module.css";
 
-const URL = "https://api.spoonacular.com/recipes/complexSearch";
-const API_KEY = "722dda5fd2504ab3a3faf9893731bffa";
+const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+const URL = import.meta.env.VITE_REACT_APP_API_URL;
 
-export default function Search({foodData, setFoodData}) {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
 
-  useEffect(() => {
-    async function fetchFood() {
-        const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
-        const data = await res.json();
-        console.log(data.results);
-        setFoodData(data.results);
-      }
+  //   useEffect(() => {
+  //     async function fetchFood() {
+  //         const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+  //         const data = await res.json();
+  //         console.log(data.results);
+  //         setFoodData(data.results);
+  //       }
 
-    fetchFood();
-  }, [query]);
+  //     fetchFood();
+  //   }, [query]);
 
   return (
-    <div>
-      <input
+    <div className={styles.searchContainer}>
+      <input className={styles.input}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
